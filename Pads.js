b0b61f4f687ee296e85848fc08ad3e1f1d261ca1
RIGOULET Henri-Pierre <oyo1505@hotmail.fr>
2019-06-13 16:34:38 +0200

    css flex

--- Pads.js
+++ Pads.js
@@ -13,7 +13,18 @@ import React from 'react';
 	render() {
 		return (
 			<div className="pads">
-
+				<div className="pads-buttons">
+					<button className="btn-pads icon-pads" >Rewind</button>
+					<button className="btn-pads icon-pads" >Forward</button>
+					<button className="btn-pads icon-pads" >Stop</button>
+					<button className="btn-pads icon-pads" >Play</button>
+				</div>
+				<div className="pads-buttons">
+					<button className="btn-pads letters-pads" >Q</button>
+					<button className="btn-pads letters-pads" >S</button>
+					<button className="btn-pads letters-pads" >D</button>
+					<button className="btn-pads letters-pads" >F</button>
+				</div>
 			</div>
 		);
 	}
